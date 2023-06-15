d6fdf4a67fa75d66749059bd7792d13881dd989b
Leonardo Cavagnis <leonardo.cavagnis@hotmail.it>
2023-06-15 09:12:26 +0200

    add CDC example

---
+++ examples/CDC_Serial/CDC_Serial.ino
@@ -0,0 +1,20 @@
+#include "USBHost_H7.h"
+
+USBHost_H7 usbhost;
+CDCSerial host;
+
+void setup() {
+  Serial.begin(115200);
+  while (!Serial);
+  Serial.println("USB CDC Serial");
+
+  usbhost.begin();
+  host.begin();
+}
+
+void loop() {
+  while (host.available()) {
+    auto _char = host.read();
+    Serial.write(_char);
+  }
+}
--- src/usbh_conf.h
+++ src/usbh_conf.h
@@ -73,7 +73,7 @@
 #define USBH_KEEP_CFG_DESCRIPTOR      1U
 
 /*----------   -----------*/
-#define USBH_MAX_NUM_SUPPORTED_CLASS      4U /* NOTE: Reduced to 4 */
+#define USBH_MAX_NUM_SUPPORTED_CLASS      5U 
 
 /*----------   -----------*/
 #define USBH_MAX_SIZE_CONFIGURATION      256U
